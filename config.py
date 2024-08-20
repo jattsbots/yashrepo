e2c451367d168baf27fb2f6e2916e37ece1b6fc7
Anmol Singh <bxjjjzid@gmail.com>
2024-08-21 00:11:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER", "6643388068")
+    OWNER = os.environ.get("OWNER", "6052303737")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD", "AS_1718")
+    PASSWORD = os.environ.get("PASSWORD", "X1BOTS)
     DATABASE_URL = os.environ.get("DATABASE_URL")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
